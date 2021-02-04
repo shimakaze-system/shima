1e8dcc7135f8c45b23054adffe046be0459d2610
gyara <me@gyara.moe>
2021-02-04 14:46:09 +0900

    config and data store

--- seiran/src/database.rs
+++ seiran/src/database.rs
@@ -1,1 +1,23 @@
+use crate::meta::MetaTable;
+use anyhow::Result;
+use std::{borrow::Cow, fs, path};
 
+const DB: &str = "data.json";
+
+pub fn load<'a>(data_dir: Cow<'a, path::Path>) -> Result<MetaTable> {
+    let db_path = data_dir.join(DB);
+    let db = fs::File::open(db_path)?;
+    Ok(serde_json::from_reader(&db)?)
+}
+
+pub fn save<'a>(data_dir: Cow<'a, path::Path>, data: Cow<'a, MetaTable>) -> Result<()> {
+    fs::create_dir_all(&data_dir).ok();
+    let db_path = data_dir.join(DB);
+    let db = fs::File::with_options()
+        .truncate(true)
+        .write(true)
+        .create(true)
+        .open(db_path)?;
+    serde_json::to_writer_pretty(db, data.as_ref())?;
+    Ok(())
+}
